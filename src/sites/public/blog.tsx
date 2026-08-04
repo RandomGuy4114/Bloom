da3ac1f57cbf7ae5a8933ce13ac5d5d5f8340de5
Liam <liam.macgregor.aguayo@gmail.com>
2026-08-03 21:22:19 -0300

    New Blog Post, That's pretty much it.

--- src/sites/public/blog.tsx
+++ src/sites/public/blog.tsx
@@ -3,6 +3,7 @@ import InfoPageHead from "../../components/InfoPageHead"
 import BlogPost from "../../components/blog/blogPost"
 import BottomBar from "../../components/BottomBar"
 import react from "../../Assets/react.png"
+import newBloom from "../../Assets/newBloom.png"
 
 export const pagePath = "/pages/blog/"
 
@@ -32,6 +33,13 @@ export default function PagesBlogPage() {
                 subtitle="Hello Bloom users! I have an exciting announcement to make: Bloom is now built using React! This means that the website is now more dynamic, interactive, and user-friendly than ever before. With React, we can make a more seamless experience for our users, and making new features and updates will be much easier. I can't wait for you all to explore the new Bloom and see what we've been working on. Thank you for your continued support, and I hope you enjoy the new Bloom experience!"
                 date="July 17, 2026"
                 image={react}
+            />
+            <BlogPost
+                title="Huge Changes Coming To Bloom!"
+                subtitle="In the coming weeks, Bloom will have a fresh new look! Focusing on user satisfaction and ease of use. I hope you all enjoy the new website as much as I enjoyed making it. Thank you for your continued support, and I can't wait to share the new Bloom with you all!"
+                date="August 3, 2026"
+                image={newBloom}
+                
             />
             <BottomBar />
         </PageLifecycle>
